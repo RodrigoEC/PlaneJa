e60d1a5d31c89406584a12d9be0e8611f272714b
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2023-08-21 14:29:00 -0300

    Gambiarra

---
+++ setup-stress.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Installing docker
+
+sudo yum install docker
+
+sudo usermod -a -G docker ec2-user
+
+id ec2-user
+
+newgrp docker
+
+sudo systemctl enable docker.service
+
+sudo systemctl start docker.service
+
+# Installing and running node_exporter
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.0/node_exporter-1.6.0.linux-amd64.tar.gz
+
+tar xvfz node_exporter-1.6.0-amd64.tar.gz
+
+nohup ./node_exporter-1.6.0-amd64/node_exporter &
+
